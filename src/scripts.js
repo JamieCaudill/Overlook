866327261f46de83e2e5fa4c16691b1d58306f99
Jamie Caudill <j.caudill7177@gmail.com>
2023-06-07 15:42:10 -0700

    feat: Fetch customers, rooms, and bookings data

--- src/scripts.js
+++ src/scripts.js
@@ -4,8 +4,45 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// An example of how you tell webpack to use an image 
+// (also need to link to it in the index.html)
 import './images/turing-logo.png'
+
+const fetchCustomers = fetch('http://localhost:3001/api/v1/customers');
+const fetchRooms = fetch('http://localhost:3001/api/v1/rooms');
+const fetchBookings = fetch('http://localhost:3001/api/v1/bookings');
+
+let customersData = [];
+let roomsData = [];
+let bookingsData = [];
+
+window.addEventListener('load', () => {
+  Promise.all([fetchCustomers, fetchRooms, fetchBookings])
+    .then(responses => {
+      responses.forEach(response => {
+        if (response.ok) {
+          response.json()
+            .then(data => {
+              if (response.url.includes('/customers')) {
+                customersData.push(data.customers)
+                console.log(customersData)
+              } else if (response.url.includes('/rooms')) {
+                roomsData.push(data.rooms)
+              } else if (response.url.includes('/bookings')) {
+                bookingsData.push(data.bookings)
+              }
+            })
+            .catch(error => {
+              console.error('Error parsing response:', error);
+            });
+        } else {
+          alert(`${response.status} server request failed, try again later`)
+          console.error('Request failed with status:', response.status);
+        }
+      });
+    });
+});
+
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
