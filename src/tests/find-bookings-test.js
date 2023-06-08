b40461363621b0dd4b6e03f90630a0cbcde6dca3
Jamie Caudill <j.caudill7177@gmail.com>
2023-06-08 11:04:30 -0700

    feat: Add sample object for testing

--- src/tests/find-bookings-test.js
+++ src/tests/find-bookings-test.js
@@ -4,7 +4,25 @@ import { sampleCustomers, sampleRooms, sampleBookings } from '../../sample-data/
 describe('findBookings', () => {
   it('should take in a specific user and return rooms booked', () => {
     const customer = sampleCustomers[0];
-    expect(customer.name).to.equal("Leatha Ullrich")
+    const booking =
+      {
+        bookingDetails: {"id": "5fwrgu4i7k55hl6t8",
+          "userID": 1,
+          "date": "2022/02/05",
+          "roomNumber": 12
+        },
+        roomDetails: {
+          "number": 12,
+          "roomType": "single room",
+          "bidet": false,
+          "bedSize": "twin",
+          "numBeds": 2,
+          "costPerNight": 172.09
+        }
+      }
+    const foundBookings = findBookings(customer, sampleRooms, sampleBookings)
+    expect(foundBookings.length).to.equal(1)
+    expect(foundBookings[0]).to.equal(booking)
   })
 })
-console.log(sampleCustomers)
+
