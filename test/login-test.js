570aa79c2ddfaf05bf37423257ea842b61828814
Jamie Caudill <j.caudill7177@gmail.com>
2023-06-09 08:25:50 -0700

    feat: Write first test for login

--- test/login-test.js
+++ test/login-test.js
@@ -5,3 +5,11 @@ import findTotalCost from "../src/functions/find-total-cost";
 import findBookings from '../src/functions/find-bookings';
 import { userLogin } from '../src/functions/login';
 import { sampleCustomers, sampleRooms, sampleBookings } from '../sample-data/sample-data';
+
+describe('userLogin', () => {
+  it('should take in a username and match it to a user', () => {
+    const username = customer01;
+    const customer = userLogin(username, sampleCustomers)
+    expect(customer.name).to.equal("Leatha Ullrich")
+  }) 
+})
