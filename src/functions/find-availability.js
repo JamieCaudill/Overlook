50d1032242e21199e12d3d96c3e112de3488b244
Jamie Caudill <j.caudill7177@gmail.com>
2023-06-08 19:47:25 -0700

    feat: Clean up

--- src/functions/find-availability.js
+++ src/functions/find-availability.js
@@ -3,9 +3,8 @@
 const findAvailability = (roomNumberInput, bookingsData) => {
   const filteredBookings = bookingsData
     .filter(booking => booking.roomNumber === roomNumberInput)
-    .map(data => data.date)
-    console.log(filteredBookings)
+    .map(data => data.date);
   return filteredBookings;
-}
+};
   
 export default findAvailability;
